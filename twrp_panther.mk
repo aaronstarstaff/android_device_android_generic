71bbd598a3d57f3d52f70b661039a3c3e1e73b48
aaronstarstaff <159981647+aaronstarstaff@users.noreply.github.com>
2025-05-10 20:38:23 -0700

    Update and rename omni_generic.mk to twrp_panther.mk

--- twrp_panther.mk
+++ twrp_panther.mk
@@ -10,16 +10,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from generic device
-$(call inherit-product, device/android/generic/device.mk)
+$(call inherit-product, device/google/panther/device.mk)
 
-PRODUCT_DEVICE := generic
-PRODUCT_NAME := omni_generic
-PRODUCT_BRAND := Android
-PRODUCT_MODEL := mainline
-PRODUCT_MANUFACTURER := android
+PRODUCT_DEVICE := panther
+PRODUCT_NAME := twrp_panther
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Pixel 7
+PRODUCT_MANUFACTURER := google
 
 PRODUCT_GMS_CLIENTID_BASE := android-google
 
